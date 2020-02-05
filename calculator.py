901c12b3eb270fb9cd1f051f0e2254ec4a3a4c4e
Osama Sider <osamamksider@hotmail.com>
2020-02-05 11:44:25 +0200

    This is a calculator program

--- calculator.py
+++ calculator.py
@@ -1,9 +1,29 @@
 
 def main():
 	#write your code here
-	pass
-	
+	num1 = input("Enter your first number :")
+	oper = input("Enter the operation ( * , / , + , - ) :")
+	num2 = input("Enter the second number :")
+	ans = 0
+	if num1.isdigit() == False or num2.isdigit() == False:
+		print(" the numbers were invalid, Try again")
+		main()
 
+	if oper == "+":
+		ans = int(num1) + int(num2)
+		print("The add of your numbers is :" ,str(ans))
+	elif oper == "-":
+		ans = int(num1) - int(num2)
+		print("The subtract of your number is: " , str(ans))
+	elif oper == "*":
+		ans = int(num1) * int(num2)
+		print("The multi of your numbers is :" , str(ans))
+	elif oper == "/" :
+		ans = int(num1) / int(num2)
+		print("The div of your numbers is :", str(ans))
+	else :
+		print("The operation is not valid , Try again")
+		main()
 
 
 if __name__ == '__main__':
